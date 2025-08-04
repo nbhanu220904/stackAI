95762c4d48d9b34a6b00a147e0a2c7ed23f3107b
nbhanu220904 <n.bhanudurgaprasad2209@gmail.com>
2025-08-04 22:04:46 +0530

    Completed StackAI Dashboard initially with some sample data

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { dummyCreationData } from "../assets/assets";
+import { GemIcon, SparklesIcon } from "lucide-react";
+import { Protect } from "@clerk/clerk-react";
+import CreationItem from "../components/CreationItem";
 
 const Dashboard = () => {
+  const [creations, setCreations] = useState([]);
+
+  const getDashboardData = async () => {
+    setCreations(dummyCreationData);
+  };
+
+  useEffect(() => {
+    getDashboardData();
+  }, []);
+
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className="h-full overflow-y-scroll p-6">
+      <div className="flex justify-start gap-4 flex-wrap">
+        {/* Creating a Total Creation Card */}
+        <div className="flex justify-between items-center w-72 p-4 px-6 bg-white rounded-xl border border-gray-200">
+          <div className="text-slate-600">
+            <p className="text-sm">Total Creations</p>
+            <h1 className="text-xl font-semibold">{creations.length}</h1>
+          </div>
+          <div className="w-10 h-10 rounded-lg bg-gradient-to-br from-[#3588f2] to-[#0bb0d7] text-white flex justify-center items-center">
+            <SparklesIcon className="w-5 text-white" />
+          </div>
+        </div>
+        {/* Creating a Active Plan Card */}
+        <div className="flex justify-between items-center w-72 p-4 px-6 bg-white rounded-xl border border-gray-200">
+          <div className="text-slate-600">
+            <p className="text-sm">Active Plan</p>
+            <h1 className="text-xl font-semibold">
+              <Protect plan="premium" fallback="Free">
+                Premium
+              </Protect>
+            </h1>
+          </div>
+          {/* <div className="w-10 h-10 rounded-lg bg-gradient-to-br from-[#004e92] to-[#0d1452] text-white flex justify-center items-center">
+           */}
+          <div className="w-10 h-10 rounded-lg bg-gradient-to-br from-[#ff61c5] to-[#9e53ee] text-white flex justify-center items-center">
+            <GemIcon className="w-5 text-white" />
+          </div>
+        </div>
+      </div>
+      <div className="space-y-3">
+        <p className="mt-6 mb-4">Recent Creations</p>
+        {creations.map((item) => (
+          <CreationItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
