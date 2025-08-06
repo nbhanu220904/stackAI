7676b25c45d3d24243ccff21c07972257b5f9c20
nbhanu220904 <n.bhanudurgaprasad2209@gmail.com>
2025-08-06 22:28:29 +0530

    Created the UI page for Generate Images Page and it is fully responsive

--- client/src/pages/GenerateImages.jsx
+++ client/src/pages/GenerateImages.jsx
@@ -1,9 +1,95 @@
-import React from "react";
+import { HashIcon, ImageIcon, SparklesIcon } from "lucide-react";
+import React, { useState } from "react";
 
 const GenerateImages = () => {
+  const imageStyles = [
+    "Realistic",
+    "Ghibili Style",
+    "3D Style",
+    "Anime style",
+    "Cartoon style",
+    "Fantacy style",
+    "Realistic style",
+    "Portrait style",
+  ];
+  const [selectedStyle, setSelectedStyle] = useState("Realistic");
+  const [input, setInput] = useState("");
+  const [publish, setPublish] = useState(false);
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <div>
-      <h1>Generate Images</h1>
+    <div className="h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700">
+      {/* Write Article Input Left Column */}
+      <form
+        onSubmit={onSubmitHandler}
+        className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200"
+      >
+        <div className="flex items-center gap-3">
+          <SparklesIcon className="w-6 text-[#004e92]" />
+          <h1 className="text-xl font-semibold">AI Image Generator</h1>
+        </div>
+        <p className="mt-6 text-sm font-medium">Describe Your Image</p>
+        <textarea
+          onChange={(event) => setInput(event.target.value)}
+          value={input}
+          rows={5}
+          className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300"
+          placeholder="Ex: A futuristic city skyline at sunset with flying cars and glowing neon lights."
+          required
+        />
+        <p className="mt-4 text-sm font-medium">Styles</p>
+        <div className="mt-3 flex gap-3 flex-wrap sm:max-w-9/11">
+          {imageStyles.map((item) => (
+            <span
+              onClick={() => setSelectedStyle(item)}
+              key={item}
+              className={`text-xs px-4 py-1 border rounded-full cursor-pointer ${
+                selectedStyle === item
+                  ? "bg-white text-blue-900"
+                  : "text-gray-500 border-gray-300"
+              }`}
+            >
+              {item}
+            </span>
+          ))}
+        </div>
+        <div className="my-6 flex items-center gap-2">
+          <label className="relative cursor-pointer">
+            <input
+              type="checkbox"
+              onChange={(event) => setPublish(event.target.checked)}
+              checked={publish}
+              className="sr-only peer"
+            />
+            <div className="w-9 h-5 bg-slate-300 rounded-full peer-checked:bg-[#004e92] transition"></div>
+            <span className="absolute left-1 top-1 w-3 h-3 bg-white rounded-full transition peer-checked:translate-x-4"></span>
+          </label>
+          <p className="text-sm">Make this Image public</p>
+        </div>
+
+        <button className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#004e92] to-[#0d1452] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer">
+          <ImageIcon className="w-5" />
+          Generate Image
+        </button>
+      </form>
+      {/* Write Article Input Right Column */}
+      <div className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96">
+        <div className="flex items-center gap-3">
+          <ImageIcon className="w-5 h-5 text-[#004e92]" />
+          <h1 className="text-xl font-semibold">Generated Image</h1>
+        </div>
+        <div className="flex-1 flex justify-center items-center">
+          <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
+            <ImageIcon className="w-9 h-9" />
+            <p>
+              Paint your vision – describe an image and click 'Generate Image'
+              to bring it to life.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
