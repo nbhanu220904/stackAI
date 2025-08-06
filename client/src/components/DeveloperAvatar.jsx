7d40d10478455fe336a00cb7f6c6b866566471b5
nbhanu220904 <n.bhanudurgaprasad2209@gmail.com>
2025-08-06 21:00:11 +0530

    Changes done in Developer Avatar Section

--- client/src/components/DeveloperAvatar.jsx
+++ client/src/components/DeveloperAvatar.jsx
@@ -4,9 +4,9 @@ import devAvatar from "../assets/img-avatar.jpg"; // Adjust path if needed
 const DeveloperAvatar = () => {
   return (
     <div className="mt-10 flex justify-center">
-      <div className="flex items-center gap-4 backdrop-blur-sm px-4 py-2 rounded-xl sm:flex flex-col sm:items-start sm:gap-2">
+      <div className="flex flex-row items-center gap-4 backdrop-blur-sm px-4 py-2 rounded-xl">
         {/* Avatar Image */}
-        <div className="w-16 h-16 rounded-full border-2 border-white shadow-md overflow-hidden scale-95 hover:scale-105 transition-transform duration-300">
+        <div className="w-16 h-16 rounded-full border-2 border-white shadow-md overflow-hidden transform scale-95 hover:scale-105 transition-transform duration-300">
           <img
             src={devAvatar}
             alt="Developer"
@@ -15,18 +15,8 @@ const DeveloperAvatar = () => {
         </div>
 
         {/* Text */}
-        {/* <span className="text-sm sm:text-base font-medium text-gray-800">
-          Developed by <strong>BHANU</strong>
-        </span> */}
-        {/* <span className="text-sm sm:text-base font-medium text-gray-700">
-          Crafted with care by{" "}
-          <strong className="text-blue-600 font-semibold">Bhanu</strong>
-        </span> */}
         <span className="text-sm sm:text-base font-medium text-gray-700">
           Designed and Developed by{" "}
-          {/* <strong className="text-blue-600 font-semibold hover:text-blue-700 duration-300 cursor-pointer hover:underline transition-all ease-in-out">
-            BHANU
-          </strong> */}
           <strong className="relative text-blue-600 font-semibold cursor-pointer group">
             BHANU
             <span className="absolute left-0 -bottom-1 h-[2px] w-0 bg-blue-600 transition-all duration-300 group-hover:w-11"></span>
