55721ad1c8844f3e4357b907bf565d23ad74d1e2
nbhanu220904 <n.bhanudurgaprasad2209@gmail.com>
2025-08-15 21:28:31 +0530

    Completed Generate Image page both frontend and Backend and also updated .env file with live backend url

--- client/src/pages/GenerateImages.jsx
+++ client/src/pages/GenerateImages.jsx
@@ -1,5 +1,11 @@
 import { HashIcon, ImageIcon, SparklesIcon } from "lucide-react";
 import React, { useState } from "react";
+import axios from "axios";
+import { useAuth } from "@clerk/clerk-react";
+
+import toast from "react-hot-toast";
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 const GenerateImages = () => {
   const imageStyles = [
@@ -15,8 +21,35 @@ const GenerateImages = () => {
   const [selectedStyle, setSelectedStyle] = useState("Realistic");
   const [input, setInput] = useState("");
   const [publish, setPublish] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [content, setContent] = useState("");
+  const { getToken } = useAuth();
+
   const onSubmitHandler = async (event) => {
     event.preventDefault();
+    try {
+      setLoading(true);
+      const prompt = `Generate an image in the style of ${selectedStyle} based on the description: ${input}.`;
+      const { data } = await axios.post(
+        "/api/ai/generate-image",
+        { prompt, publish },
+        {
+          headers: {
+            Authorization: `Bearer ${await getToken()}`,
+          },
+        }
+      );
+      if (data.success) {
+        setContent(data.content);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -69,8 +102,15 @@ const GenerateImages = () => {
           <p className="text-sm">Make this Image public</p>
         </div>
 
-        <button className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#004e92] to-[#0d1452] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer">
-          <ImageIcon className="w-5" />
+        <button
+          disabled={loading}
+          className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#004e92] to-[#0d1452] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer"
+        >
+          {loading ? (
+            <span className="w-4 h-4 my-1 rounded-full border-2 border-t-transparent animate-spin"></span>
+          ) : (
+            <ImageIcon className="w-5" />
+          )}
           Generate Image
         </button>
       </form>
@@ -80,15 +120,21 @@ const GenerateImages = () => {
           <ImageIcon className="w-5 h-5 text-[#004e92]" />
           <h1 className="text-xl font-semibold">Generated Image</h1>
         </div>
-        <div className="flex-1 flex justify-center items-center">
-          <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
-            <ImageIcon className="w-9 h-9" />
-            <p>
-              Paint your vision – describe an image and click 'Generate Image'
-              to bring it to life.
-            </p>
+        {!content ? (
+          <div className="flex-1 flex justify-center items-center">
+            <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
+              <ImageIcon className="w-9 h-9" />
+              <p>
+                Paint your vision – describe an image and click 'Generate Image'
+                to bring it to life.
+              </p>
+            </div>
           </div>
-        </div>
+        ) : (
+          <div className="flex-1 flex justify-center items-center mt-3 h-full">
+            <img src={content} alt="Generated" className="w-full h-full" />
+          </div>
+        )}
       </div>
     </div>
   );
