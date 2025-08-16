d6d7f552938eb05ba2c0e23ae5dcdbd7c4e01dcd
nbhanu220904 <n.bhanudurgaprasad2209@gmail.com>
2025-08-16 22:51:30 +0530

    Updated sidebar component

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -37,40 +37,18 @@ const SideBar = ({ sidebar, setSidebar }) => {
 
   return (
     <div
-      className={`w-60 bg-white border-r border-gray-200 flex flex-col justify-between items-center max-sm:absolute relative z-0 top-14 bottom-0 ${
+      className={`w-60 bg-white border-r border-gray-200 flex flex-col justify-between items-center max-sm:absolute relative z-50 top-0 bottom-0 ${
         sidebar ? "translate-x-0" : "max-sm:-translate-x-full"
       } sm:translate-x-0 transition-all duration-300 ease-in-out`}
     >
-      <div className="my-7 w-full">
+      <div className="my-3 w-full">
         <img
           src={user.imageUrl}
           alt="user-image"
           className="w-13 rounded-full mx-auto"
         />
         <h1 className="mt-2 text-center">{user.fullName}</h1>
-        <div className="px-5 mt-5 text-sm text-gray-600 font-medium">
-          {/* {navItems.map(({ to, label, Icon }) => {
-            <NavLink
-              key={to}
-              to={to}
-              end={to === "/ai"}
-              onClick={() => setSidebar(false)}
-              className={({ isActive }) =>
-                `px-3.5 py-2.5 flex items-center gap-3 rounded ${
-                  isActive
-                    ? "bg-gradient-to-r from-[#3c81f6] to-[#9234ea] text-white"
-                    : ""
-                }`
-              }
-            >
-              {({ isActive }) => (
-                <>
-                  <Icon className={`w-4 h-4 ${isActive ? "text-white" : ""}`} />
-                  {label}
-                </>
-              )}
-            </NavLink>;
-          })} */}
+        <div className="px-5 mt-2 text-sm text-gray-600 font-medium">
           {navItems.map(({ to, label, Icon }) => (
             <NavLink
               key={to}
