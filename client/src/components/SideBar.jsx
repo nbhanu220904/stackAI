0c72db8746934b78f17f616b687a454dd53ccfc0
nbhanu220904 <n.bhanudurgaprasad2209@gmail.com>
2025-08-16 18:32:41 +0530

    updated SideBar component

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -37,7 +37,7 @@ const SideBar = ({ sidebar, setSidebar }) => {
 
   return (
     <div
-      className={`w-60 bg-white border-r border-gray-200 flex flex-col justify-between items-center max-sm:absolute top-14 bottom-0 ${
+      className={`w-60 bg-white border-r border-gray-200 flex flex-col justify-between items-center max-sm:absolute relative z-0 top-14 bottom-0 ${
         sidebar ? "translate-x-0" : "max-sm:-translate-x-full"
       } sm:translate-x-0 transition-all duration-300 ease-in-out`}
     >
